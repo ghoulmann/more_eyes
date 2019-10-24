8ff7b3de221320270177baf0eb7a39c705a37556
Rik Goldman <rik@goldman@gmail.com>
2019-10-24 09:57:19 -0400

    nlp/process has unfinished wordtoken_no_punct function (bugs). Requirements has bad packages removed.

--- nlp/process.py
+++ nlp/process.py
@@ -1,5 +1,6 @@
 import spacy
 from spacy_readability import Readability
+from string import punctuation
 
 '''
 def process(text):
@@ -13,6 +14,7 @@ class NLP():
         self.nlp.add_pipe(Readability(), last=True)
         self.doc = self.nlp(text)
         self.readability = self.readability_indexes()
+        self.word_tokens = self.tokenize_words(self.doc)
     def readability_indexes(self):
         self.readability_scores = {}
         self.readability_scores['ari'] = self.doc._.automated_readability_index
@@ -22,6 +24,25 @@ class NLP():
         self.readability_scores['flesch_kincaid_re'] = self.doc._.flesch_kincaid_reading_ease
         self.readability_scores['forcast'] = self.doc._.forcast
         return self.readability_scores
+    def tokenize_words(self, document):
+        spacy_word_tokens =  [t.text for t in document]
+        no_punct_word_tokens = []
+        for w in spacy_word_tokens:
+            for p in punctuation:
+                if p in w:
+                    if w == p:
+                        pass
+                    elif p in w:
+                        w.replace(p, "")
+                        no_punct_word_tokens.append(w)
+                    else:
+                        pass
+                else:
+                    no_punct_word_tokens.append(w)
+                    
+                
+
+        return (spacy_word_tokens, no_punct_word_tokens)
 
 
 
