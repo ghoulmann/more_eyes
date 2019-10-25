8ed529f9897e313415d43f91c7e7a1430b3cbbae
Rik Goldman <rikgoldman@gmail.com>
2019-10-25 06:32:22 -0400

    readability method vars work

--- nlp/process.py
+++ nlp/process.py
@@ -10,22 +10,25 @@ def process(text):
     return nlp(text)
 '''
 class NLP():
+    nlp = spacy.load('en_core_web_sm')
+    nlp.add_pipe(Readability(), last=True)
+    
     def __init__(self, text):
-        self.nlp = spacy.load('en_core_web_sm')
-        self.nlp.add_pipe(Readability(), last=True)
+        
+        #self.nlp.add_pipe(Readability(), last=True)
         self.doc = self.nlp(text)
         self.readability = self.readability_indexes()
         self.word_tokens = self.tokenize_words(self.doc)
         self.polysyllables = self.get_polysyllables(self.word_tokens[1])
     def readability_indexes(self):
-        self.readability_scores = {}
-        self.readability_scores['ari'] = self.doc._.automated_readability_index
-        self.readability_scores['coleman_liau_index'] = self.doc._.coleman_liau_index
-        self.readability_scores['dale_chall'] = self.doc._.dale_chall
-        self.readability_scores['flesch_kincaid_grade'] = self.doc._.flesch_kincaid_grade_level
-        self.readability_scores['flesch_kincaid_re'] = self.doc._.flesch_kincaid_reading_ease
-        self.readability_scores['forcast'] = self.doc._.forcast
-        return self.readability_scores
+        readability_scores = {}
+        readability_scores['ari'] = self.doc._.automated_readability_index
+        readability_scores['coleman_liau_index'] = self.doc._.coleman_liau_index
+        readability_scores['dale_chall'] = self.doc._.dale_chall
+        readability_scores['flesch_kincaid_grade'] = self.doc._.flesch_kincaid_grade_level
+        readability_scores['flesch_kincaid_re'] = self.doc._.flesch_kincaid_reading_ease
+        readability_scores['forcast'] = self.doc._.forcast
+        return readability_scores
     def tokenize_words(self, document):
         self.spacy_word_tokens = [t.text for t in document]
         self.no_punct_word_tokens = []
