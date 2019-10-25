e41c8bb082490689309a7cd85a674beefd260054
Rik Goldman <rik@goldman@gmail.com>
2019-10-25 11:39:06 -0400

    parse argument for filepath

--- more_eyes.py
+++ more_eyes.py
@@ -1,5 +1,10 @@
+from sys import argv
+
 from input.get_document import DataFile
 from nlp.process import NLP
 
-document = DataFile(file-data)
+document = DataFile(argv[1])
 nat_lang = NLP(document.content)
+
+print(document.__dict__)
+print(nat_lang.__dict__)
